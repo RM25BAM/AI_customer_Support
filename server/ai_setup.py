67f1bf10af5e01bbc9b98757f74b902e6275d380
SRshohan <srshohan02@gmail.com>
2024-08-12 12:06:44 -0400

    Created requirements.txt, setup venv and api keys

--- server/ai_setup.py
+++ server/ai_setup.py
@@ -106,3 +106,33 @@ def respond(survey):
     response = chain.invoke({"user_data": user_data})
 
     return response
+
+
+
+if __name__ == "__main__":
+    survey = {
+    "What is the gym goal": "Lose weight",
+    "How much free time you have": "10 hours per week",
+    "What is your weekly schedule looks like": {
+        "Monday": "9:00 AM - 10:30 AM (Math), 2:00 PM - 3:30 PM (History)",
+        "Tuesday": "10:00 AM - 11:30 AM (Physics), 1:00 PM - 2:30 PM (Chemistry)",
+        "Wednesday": "9:00 AM - 10:30 AM (Math), 2:00 PM - 3:30 PM (History)",
+        "Thursday": "10:00 AM - 11:30 AM (Physics), 1:00 PM - 2:30 PM (Chemistry)",
+        "Friday": "9:00 AM - 10:30 AM (English), 2:00 PM - 3:30 PM (Biology)",
+    },
+    "What is your current weight?": "70 kg",
+    "What is your height?": "170 cm",
+    "What is your preferred workout environment?": "Gym",
+    "Are there any specific exercises or activities you enjoy?": "Running, Swimming, Yoga",
+    "Do you have any injuries or physical limitations?": "Knee pain when running, lower back issues",
+    "What is your current fitness level?": "Intermediate",
+    "What is your desired intensity level for workouts?": "Medium"
+}
+
+    
+    print(respond(survey))
+
+
+
+
+
